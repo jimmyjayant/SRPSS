6fdd9d662cf537b24640466f7a939b9738113727
JIMMY JAYANT <113120690+jimmyjayant@users.noreply.github.com>
2023-09-21 11:08:54 +0530

    Added a custom error page 404.php and also a file .htaccess, also deleted a image file 'login user.png'

---
+++ 404.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en-IN">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Jimmy Jayant">
+    <meta name="keywords" content="science, research, papers, engineering, research paper">
+    <meta name="description" content="Scientific Research Paper Submission System (SRPSS) online platform allows you to submit and view research papers">
+    <link rel="icon" type="image/x-icon" href="Images/SRPSS.png">
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+    <title>
+        Scientific Research Paper Submission System (SRPSS) 
+    </title>
+</head>
+<body>
+<div class="main">
+  <div class="image">
+    <div class="eye1">
+    </div>
+    <div class="eye2">
+    </div>
+    <div class="smile">
+    </div>
+  </div>
+  <div class="msg">
+    <h1>
+      404
+    </h1>
+    <h3>
+      Page not found 
+    </h3>
+    <p>
+      The Page you are looking for doesn't exist or an other error occurred. 
+      <br>
+      Go back, or head over to srpss.com to choose a new direction. 
+    </p>
+  </div>
+</div>
+</body>
+</html>
