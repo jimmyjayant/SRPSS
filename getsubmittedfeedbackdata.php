00105a0438e2e7f385b23f1ef800bd04b5a56c0d
JIMMY JAYANT <113120690+jimmyjayant@users.noreply.github.com>
2024-02-17 22:54:09 +0530

    The user can now see the feedback submitted by him/her in the feedback section of the User dashboard. The feedback data will be shown in a table with the status of approval and the date of feedback submission.

--- getsubmittedfeedbackdata.php
+++ getsubmittedfeedbackdata.php
@@ -8,21 +8,43 @@ require 'sessionstart.php';
 include('databaseconnection.php');
 
 // sql query
-$sql = "SELECT mess from feedback WHERE email='{$_SESSION['email']}' ORDER BY 'timestamp' DESC";
+$sql = "SELECT * from feedback WHERE email='{$_SESSION['email']}' ORDER BY 'timestamp' DESC";
 
 // perform query and store result in a variable
 $result = $conn->query($sql);
 
 if($result->num_rows > 0)
 {
-    $i = 1;
+    echo "<table>";
+    echo "<tr>";
+    echo "<th>ID</th>";
+    echo "<th>Topic</th>";
+    echo "<th>Message</th>";
+    echo "<th>Approved</th>";
+    echo "<th>Submitted</th>";
+    echo "</tr>";
     while($row = $result->fetch_assoc())
     {
-        echo "<p>";
-        echo "$i {$row['mess']}";
-        echo "</p>";
-        $i++;
+        echo "<tr>";
+        echo "<td>{$row['id']}</td>";
+        echo "<td>{$row['topic']}</td>";
+        echo "<td>{$row['mess']}</td>";
+        if($row['approved'] == 0)
+        {
+            echo "<td>Not Yet</td>";
+        }
+        if($row['approved'] == 1)
+        {
+            echo "<td style='color:green;'>Yes</td>";
+        }
+        if($row['approved'] == -1)
+        {
+            echo "<td style='color:red;'>No</td>";
+        }
+        echo "<td>{$row['timestamp']}</td>";
+        echo "</tr>";
     }
+    echo "</table>";
 }
 else
 {
