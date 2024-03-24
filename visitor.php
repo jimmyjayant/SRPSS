6030b6ee47cec9f9c86848a3a8324dcdf4d73a64
JIMMY JAYANT <113120690+jimmyjayant@users.noreply.github.com>
2024-03-24 10:54:29 +0530

    Now when the php website is viewed from smartphone, the user will not get an error stating that undefined array ['REMOTE_HOST']

--- visitor.php
+++ visitor.php
@@ -13,7 +13,7 @@ else
     $email = NULL;
 }
 
-$remotehost = $_SERVER['REMOTE_HOST'] ?: gethostbyaddr($_SERVER['REMOTE_ADDR']);
+$remotehost = isset($_SERVER['REMOTE_HOST']) ? $_SERVER['REMOTE_HOST'] : gethostbyaddr($_SERVER['REMOTE_ADDR']);
 
 // store query in a variable
 $sql = "INSERT INTO visitors (device, address, port, status, email) VALUES ('$remotehost', '{$_SERVER['REMOTE_ADDR']}', '{$_SERVER['REMOTE_PORT']}', '$status', '$email')";
